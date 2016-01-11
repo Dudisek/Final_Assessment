67e2263c8b64f08d4e92eb4f95b3ee5f165ddd1d
Dudisek <peta.duda@gmail.com>
2016-01-11 09:55:58 +0800

    generate controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,16 @@
 Rails.application.routes.draw do
+  get 'bookmarks/index'
+
+  get 'bookmarks/new'
+
+  get 'bookmarks/create'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'user/new'
+
+  get 'user/create'
+
 end
--- db/migrate/20160111014657_create_bookmarks.rb
+++ db/migrate/20160111014657_create_bookmarks.rb
@@ -2,6 +2,7 @@ class CreateBookmarks < ActiveRecord::Migration
   def change
     create_table :bookmarks do |t|
       t.string :url
+      t.integer :user_id
 
       t.timestamps null: false
     end
